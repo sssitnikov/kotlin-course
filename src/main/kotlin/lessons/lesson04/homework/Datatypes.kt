c715c1932aa6349724d7cb15ec537dc7dfbb3adc
Sitnikov Sergei <sitnikov94@gmail.com>
2026-09-14 13:32:25 +0300

    add Datatypes with comments

---
+++ src/main/kotlin/lessons/lesson04/homework/Datatypes.kt
@@ -0,0 +1,52 @@
+package lessons.lesson04.homework
+
+val v1: Int = 42
+val v2: Long = 98765432123456789L
+val v3: Float = 23.45f
+val v4: Double = 0.123456789
+val v5: String = "Kotlin & Java"
+// Ошибка связана с тем, что нужно использовать нижний регистр для Boolean. val v6: Boolean = false
+val v6: Boolean = FALSE
+val v7: Char = 'c'
+val v8: Int = 500
+val v9: Long = 4294967296L
+val v10: Float = 18.0f
+val v11: Double = -0.001
+val v12: String = "OpenAI"
+val v13: String = "true"
+val v14: List<Int> = listOf(3, 14)
+val v15: Char = '9'
+val v16: Int = 2048
+val v17: Long = 10000000000L
+val v18: Set<String> = setOf("OpenAI", "Quantum Computing")
+val v19: Float = 5.75f
+// Ошибка с тем, что нужно инициализовать и задать переменную как val v20: Double = 1.414 или как val v20: String = "1.414"
+val v20: Double = `1.414`
+val v21: String = "Artificial Intelligence"
+val v22: Array<Any> = arrayOf('x', "A")
+val v23: String = "Android Studio"
+val v24: Char = '@'
+val v25: Int = 1024
+val v26: Long = 1234567890123L
+val v27: Float = 10.01f
+val v28: Double = -273.15
+val v29: String = "SpaceX"
+// Ошибка связана с тем, что нужно использовать нижний регистр для Boolean val v30: Boolean = false
+val v30: Boolean = FALSE
+val v31: Double = 0.007
+// Ошибка связана с тем, что используются некорректные ковычки, необходимо val v32: String = "🤯"
+val v32: String = “🤯”
+val v33: Map<String, Int> = mapOf("true" to 2, "false" to 34)
+// Ошибка связана с тем, что нужно инициализовать и задать переменную как val v34: String = "65535" или val v34: Int = 65535
+val v34: String = ‘65535’
+val v35: Long = 72057594037927935L
+val v36: Float = 2.71828f
+val v37: Double = 101.0101
+val v38: String = "Quantum Computing"
+val v39: Map<Int, String> = mapOf(2 to "true", 34 to "false")
+val v40: Char = 'x'
+val v41: Int = 314
+val v42: Long = 123456789123456789L
+val v43: Float = 6.626f
+//Ошибка связана с тем, что нужно использовать нижний регистр для Boolean. val v44: Boolean = true
+val v44: Boolean = TRUE
